5649d0a2ef4a2b076ae301a24210c3cf467b469d
yangdangfu <yangdangfu@csu.edu.cn>
2021-08-24 11:18:25 +0800

    modify schedule frequency

--- main_sync.py
+++ main_sync.py
@@ -9,23 +9,27 @@ import schedule
 import time
 
 
-def sync(sync_info_csv: str):
-    sync_info = pd.read_csv(sync_info_csv)
-    sync_info.fillna("", inplace=True)
+def sync(sync_info: pd.DataFrame):
+    print(
+        f"{datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')} Scheduled sync start..."
+    )
 
     downloaders: List[FTPDownloader] = list()
     for item in sync_info.itertuples(index=False):
         host, user, passwd, cwd, local_root, file_reg = item
 
-        print(host, user, passwd, cwd, local_root, file_reg)
+        # print(host, user, passwd, cwd, local_root, file_reg)
 
         downloaders.append(
-            FTPDownloader(host=host,
-                          cwd=cwd,
-                          local_root=local_root,
-                          file_reg=file_reg,
-                          user=str(user),
-                          passwd=str(passwd)))
+            FTPDownloader(
+                host=host,
+                cwd=cwd,
+                local_root=local_root,
+                file_reg=file_reg,
+                user=str(user),
+                passwd=str(passwd),
+            )
+        )
 
     # for downloader in downloaders:
     #     try:
@@ -34,7 +38,7 @@ def sync(sync_info_csv: str):
     #         break  # stop the download if the `ctrl+c` is pressed
     #     except:
     #         print(f"Something wrong!")
-    pool = mp.Pool(4)
+    pool = mp.Pool(12)
     pools = [pool.apply_async(downloader.run) for downloader in downloaders]
     pool.close()
     for p in pools:
@@ -43,6 +47,7 @@ def sync(sync_info_csv: str):
 
 if __name__ == "__main__":
     import sys
+
     argv = sys.argv
 
     # Determine CSV file that the sync info come from
@@ -50,21 +55,22 @@ if __name__ == "__main__":
     if len(argv) == 2:
         csv = argv[1]
     elif len(argv) != 1:
-        assert os.path.exists(
-            csv), f"Invalid input command line arguments {argv[1:]}."
+        assert os.path.exists(csv), f"Invalid input command line arguments {argv[1:]}."
     assert os.path.exists(csv), f"CSV file {csv} doesn't exist."
 
+    sync_info_df = pd.read_csv(csv)
+    sync_info_df.fillna("", inplace=True)
     # run the sync() for 1 time ahead
     # sync(sync_info_csv=csv)
     # Try to run at 01:30 every day
+    schedule.every().day.at("00:00").do(sync, sync_info_csv=csv)
     # schedule.every().day.at("01:30:00").do(sync, sync_info_csv=csv)
-    # schedule.every().day.at("01:30:00").do(sync, sync_info_csv=csv)
-    schedule.every(1).minutes.do(sync, sync_info_csv=csv)
+    # schedule.every().minute.do(sync, sync_info=sync_info_df)
 
     while True:
         try:
             schedule.run_pending()
-            time.sleep(10)
+            time.sleep(3600)
         except KeyboardInterrupt:
             break  # stop the download if the `ctrl+c` is pressed
         except:
